6d743f230d7b631b975feb5812d0a0d5dec4d227
edsonsantos59 <edson1509@hotmail.com>
2022-06-17 17:29:24 -0300

    commit

--- projeto5/settings.py
+++ projeto5/settings.py
@@ -4,6 +4,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
+STATIC_ROOT = 'static'
+
 SECRET_KEY = 'django-insecure-u5a747r9x9w)6xh7r&hc+gjgugy0^vrfi!!vlzmr6+5!x$-7g-'
 
 DEBUG = True
